37e61c4e1872300bfe6dd95f8fdf1ee0edafeaf9
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-07-29 08:16:59 +0200

    ast: update expression types to support new data structures including strings and lists

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,18 +1,8 @@
-type binop = 
-| Plus
-| Minus
-| Times
-| Div
-
-type t = 
-| Bool_T
-| Int_T
-| Fun of t * t
-
 type expr = 
-| Bool_Literal of bool
-| Int_Literal of int
-| Lambda of {id: string; e: expr}
+| Bool of bool
+| Number of int
+| String of string
+| Symbol of string
+| List of expr list
+| Lambda of {ids: string list; e: expr}
 | If of { cond: expr; y: expr; n: expr}
-| Let of { id: string; e: expr }
-| Op of { op: binop; lhs: expr; rhs: expr }
