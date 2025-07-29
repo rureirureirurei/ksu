7f10fe7e95538833314e82ee3299607b4c627abc
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-07-29 17:21:00 +0200

    refactor: clean up formatting and improve readability in ksu.ml and ast.ml

--- bin/ksu.ml
+++ bin/ksu.ml
@@ -1,49 +1,42 @@
 (* Stub functions for different modes *)
-let repl () = 
+let repl () =
   Printf.printf "KSU REPL - Not implemented yet\n";
   Printf.printf "Type expressions to evaluate (Ctrl+C to exit)\n";
   failwith "REPL not implemented"
 
 let compile ~output files =
-  Printf.printf "Compiling to %s with files: %s\n" 
-    output (String.concat ", " files);
+  Printf.printf "Compiling to %s with files: %s\n" output
+    (String.concat ", " files);
   failwith "Compiler not implemented"
 
 (* Command line argument parsing *)
 let usage_msg = "ksu [--compile] [--output <file>] <files>..."
-
 let compile_mode = ref false
 let output_file = ref ""
 let input_files = ref []
+let anon_fun filename = input_files := filename :: !input_files
 
-let anon_fun filename = 
-  input_files := filename :: !input_files
-
-let speclist = [
-  ("--compile", Arg.Set compile_mode, "Compile to native executable");
-  ("--output", Arg.Set_string output_file, "Output file for compilation");
-]
+let speclist =
+  [
+    ("--compile", Arg.Set compile_mode, "Compile to native executable");
+    ("--output", Arg.Set_string output_file, "Output file for compilation");
+  ]
 
 let () =
   Arg.parse speclist anon_fun usage_msg;
-  
+
   let files = List.rev !input_files in
-  
+
   match (!compile_mode, files) with
-  | (true, []) ->
+  | true, [] ->
       Printf.eprintf "Error: No input files specified for compilation\n";
       Printf.eprintf "%s\n" usage_msg;
       exit 1
-  | (true, _) ->
-      if !output_file = "" then begin
+  | true, _ ->
+      if !output_file = "" then (
         Printf.eprintf "Error: --output flag required for compilation\n";
         Printf.eprintf "%s\n" usage_msg;
-        exit 1
-      end;
+        exit 1);
       compile ~output:!output_file files
-  | (false, []) ->
-      repl ()
-  | (false, _) ->
-      Interpreter.interpret files
-  
-
+  | false, [] -> repl ()
+  | false, _ -> Interpreter.interpret files
--- lib/ast.ml
+++ lib/ast.ml
@@ -1,24 +1,29 @@
-type expr = 
-| Bool of bool
-| Number of int
-| String of string
-| Symbol of string
-| App of expr list
-| Lambda of {ids: string list; body: expr}
-| If of { cond: expr; y: expr; n: expr}
-| Callcc of expr
-| Let of {ids: string list; defs: expr list; b: expr}
-| Define of {name: string; expr: expr}
-
+type expr =
+  | Bool of bool
+  | Number of int
+  | String of string
+  | Symbol of string
+  | App of expr list
+  | Lambda of { ids : string list; body : expr }
+  | If of { cond : expr; y : expr; n : expr }
+  | Callcc of expr
+  | Let of { ids : string list; defs : expr list; b : expr }
+  | Define of { name : string; expr : expr }
 
 let rec string_of_expr = function
-| Bool b -> string_of_bool b
-| Number n -> string_of_int n
-| String s -> s
-| Symbol s -> s
-| App l -> "(" ^ String.concat " " (List.map string_of_expr l) ^ ")"
-| Lambda {ids; body} -> "(lambda (" ^ String.concat " " ids ^ ") " ^ string_of_expr body ^ ")"
-| If {cond; y; n} -> "(if " ^ string_of_expr cond ^ " " ^ string_of_expr y ^ " " ^ string_of_expr n ^ ")"
-| Callcc e -> "(callcc " ^ string_of_expr e ^ ")"
-| Let {ids; defs; b} -> "(let (" ^ String.concat " " ids ^ ") (" ^ String.concat " " (List.map string_of_expr defs) ^ ") " ^ string_of_expr b ^ ")"
-| Define {name; expr} -> "(define " ^ name ^ " " ^ string_of_expr expr ^ ")"
+  | Bool b -> string_of_bool b
+  | Number n -> string_of_int n
+  | String s -> s
+  | Symbol s -> s
+  | App l -> "(" ^ String.concat " " (List.map string_of_expr l) ^ ")"
+  | Lambda { ids; body } ->
+      "(lambda (" ^ String.concat " " ids ^ ") " ^ string_of_expr body ^ ")"
+  | If { cond; y; n } ->
+      "(if " ^ string_of_expr cond ^ " " ^ string_of_expr y ^ " "
+      ^ string_of_expr n ^ ")"
+  | Callcc e -> "(callcc " ^ string_of_expr e ^ ")"
+  | Let { ids; defs; b } ->
+      "(let (" ^ String.concat " " ids ^ ") ("
+      ^ String.concat " " (List.map string_of_expr defs)
+      ^ ") " ^ string_of_expr b ^ ")"
+  | Define { name; expr } -> "(define " ^ name ^ " " ^ string_of_expr expr ^ ")"
