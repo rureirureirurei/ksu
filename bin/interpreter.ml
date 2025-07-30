ce6c820131f105e2a88bbee9f7d77808f08730ce
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-07-30 11:09:38 +0200

    refactor: enhance interpreter by implementing continuation support and refining expression evaluation logic

--- bin/interpreter.ml
+++ bin/interpreter.ml
@@ -29,6 +29,7 @@ and value =
       body : Ast.expr;
       env : env;
     }
+  | VCont of (value -> value)
 
 (* Convert values to string representation *)
 let string_of_value = function
@@ -37,6 +38,7 @@ let string_of_value = function
   | VString s -> s
   | VClosure _ -> "<function>"
   | VRecClosure _ -> "<rec-function>"
+  | VCont _ -> "<continuation>"
 
 
 let print_env env =
@@ -56,33 +58,34 @@ let rec value_to_bool = function
 (* Handle built-in operations *)
 and eval_builtin_op op args env k =
   eval_exprs env args [] (fun values ->
-      let numbers = List.map value_to_number values in
-      k
-      @@
-      match op with
-      | "+" -> VNumber (List.fold_left ( + ) 0 numbers)
-      | "=" ->
-          VBool
-            (List.length numbers = 2
-            && List.hd numbers = List.hd (List.tl numbers))
-      | "-" ->
-          VNumber (List.fold_left ( - ) (List.hd numbers) (List.tl numbers))
-      | "*" -> VNumber (List.fold_left ( * ) 1 numbers)
-      | _ -> failwith ("Unknown operation: " ^ op))
+  let numbers = List.map value_to_number values in
+  k @@ match op with
+  | "+" -> VNumber (List.fold_left ( + ) 0 numbers)
+  | "=" ->
+      VBool
+        (List.length numbers = 2
+        && List.hd numbers = List.hd (List.tl numbers))
+  | "-" ->
+      VNumber (List.fold_left ( - ) (List.hd numbers) (List.tl numbers))
+  | "*" -> VNumber (List.fold_left ( * ) 1 numbers)
+  | _ -> failwith ("Unknown operation: " ^ op)
+  )
 
 (* Evalualte a list of expressions sequentially, expressions cannot reference variables in each other similar to regular let*)
-and eval_exprs :
-    'a. env -> Ast.expr list -> value list -> (value list -> 'a) -> 'a =
+and eval_exprs : env -> Ast.expr list -> value list -> (value list -> value) -> value =
  fun env args acc k ->
   match args with
   | [] -> k acc
-  | a :: args -> eval_expr env a (fun v -> eval_exprs env args (acc @ [ v ]) k)
+  | a :: args -> 
+  eval_expr env a (fun v -> 
+    eval_exprs env args (acc @ [v]) k
+  )
 
 and extend_env : env -> (string * value) list -> env =
   List.fold_left (fun acc (name, value) -> Env.add name value acc)
 
 (* Main expression evaluation function *)
-and eval_expr : 'a. env -> Ast.expr -> (value -> 'a) -> 'a =
+and eval_expr : env -> Ast.expr -> (value -> value) -> value =
  fun env expr k ->
   match expr with
   | Ast.Number n -> k (VNumber n)
@@ -101,26 +104,37 @@ and eval_expr : 'a. env -> Ast.expr -> (value -> 'a) -> 'a =
           match func_value with
           | VClosure { args; body; env=captured_env } ->
               eval_exprs env arg_exprs [] (fun arg_values ->
-                  let extended_env =
-                    extend_env captured_env (List.combine args arg_values)
-                  in
-                  eval_expr extended_env body k)
+              let extended_env =
+                extend_env captured_env (List.combine args arg_values)
+              in
+              eval_expr extended_env body k )
           | VRecClosure { name; args; body; env=captured_env } ->
             eval_exprs env arg_exprs [] (fun arg_values ->
-              let extended_env =
-                Env.add name func_value
-                  (extend_env captured_env (List.combine args arg_values))
-              in
-              eval_expr extended_env body k)
+            let extended_env =
+              Env.add name func_value
+                (extend_env captured_env (List.combine args arg_values))
+            in
+            eval_expr extended_env body k )
+          | VCont f -> 
+            eval_exprs env arg_exprs [] (fun arg_values ->
+            f (List.hd arg_values))
+          
           | _ -> failwith "Expected a function")
   | Ast.App [] -> failwith "Empty application"
-  | Ast.Callcc _ -> failwith "Callcc not implemented"
+  | Ast.Callcc (Ast.Lambda { ids; body }) -> (
+    match ids with 
+    | [id] -> 
+      let extended_env = Env.add id (VCont k) env in 
+      eval_expr extended_env body k 
+      (* We may pass the (_ -> "failwith call/cc must end with k call" ) *)
+    | _ -> failwith "Callcc expected a lambda with one argument")
+  | Ast.Callcc _ -> failwith "Callcc expected a lambda"
   | Ast.Let { defs; body } ->
       eval_exprs env (List.map snd defs) [] (fun values ->
-          let extended_env =
-            extend_env env (List.combine (List.map fst defs) values)
-          in
-          eval_expr extended_env body k)
+      let extended_env =
+        extend_env env (List.combine (List.map fst defs) values)
+      in
+      eval_expr extended_env body k)
   | Ast.If { cond; y; n } ->
       eval_expr env cond (fun condition_value ->
           if value_to_bool condition_value then eval_expr env y k
@@ -134,7 +148,9 @@ let process_definition : string -> Ast.expr -> env -> env =
   | Ast.Lambda { ids; body } ->
       let recursive_closure = VRecClosure { name; args = ids; body; env } in
       Env.add name recursive_closure env
-  | _ -> eval_expr env expr (fun v -> Env.add name v env)
+  | _ -> 
+    let v = eval_expr env expr (fun v -> v) in
+    Env.add name v env
 
 (* Evaluate a list of top-level expressions *)
 let eval_file : Ast.expr list -> env -> value list =
@@ -146,7 +162,9 @@ let eval_file : Ast.expr list -> env -> value list =
         | Ast.Define { name; expr } ->
             let updated_env = process_definition name expr env in
             (acc, updated_env)
-        | _ -> eval_expr env expr (fun v -> (v :: acc, env)))
+        | _ -> 
+          let v = eval_expr env expr (fun v -> v) in
+          (v :: acc, env))
       ([], initial_env) exprs
   in
   List.rev results
