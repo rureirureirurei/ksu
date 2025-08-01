66d85ccf87ed972f06c50b960705bfa8def8bfd3
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-08-01 14:58:12 +0200

    refactor: improve expression string representation in AST; enhance formatting for various expression types and introduce auxiliary function for cleaner output

--- lib/ast.ml
+++ lib/ast.ml
@@ -50,32 +50,41 @@ let fresh_var: unit -> expr =
     synthetic (Var result)
 
 
-let rec string_of_expr {value; _} = match value with
+let rec string_of_expr_aux offset {value; _} = match value with
   | Bool b -> string_of_bool b
   | Number n -> string_of_int n
   | String s -> s
   | Var s -> s
-  | App l -> "(" ^ String.concat "\n  " (List.map string_of_expr l) ^ ")"
+  | App l -> 
+      let indent = String.make offset ' ' in
+      let args_str = List.map (string_of_expr_aux (offset + 2)) l in
+      "(" ^ String.concat ("\n" ^ indent ^ "  ") args_str ^ ")"
   | Lambda { ids; body } ->
-      "(lambda (" ^ String.concat " " ids ^ ")\n  " ^ string_of_expr body ^ ")"
+      let indent = String.make offset ' ' in
+      "(lambda (" ^ String.concat " " ids ^ ")\n" ^ indent ^ "  " ^ string_of_expr_aux (offset + 2) body ^ ")"
   | If { cond; y; n } ->
-      "(if " ^ string_of_expr cond ^ "\n    " ^ string_of_expr y ^ "\n    "
-      ^ string_of_expr n ^ ")"
-  | Callcc e -> "(callcc " ^ string_of_expr e ^ ")"
+      let indent = String.make offset ' ' in
+      "(if " ^ string_of_expr_aux offset cond ^ "\n" ^ indent ^ "    " ^ string_of_expr_aux (offset + 4) y ^ "\n" ^ indent ^ "    " ^ string_of_expr_aux (offset + 4) n ^ ")"
+  | Callcc e -> 
+      "(callcc " ^ string_of_expr_aux (offset + 2) e ^ ")"
   | Let { defs; body } ->
+      let indent = String.make offset ' ' in
+      let defs_str = List.map (fun (name, expr) -> name ^ " " ^ string_of_expr_aux (offset + 3) expr) defs in
       "(let ("
       ^ String.concat " " (List.map fst defs)
-      ^ ")\n  ("
-      ^ String.concat "\n   " (List.map string_of_expr (List.map snd defs))
-      ^ ")\n  " ^ string_of_expr body ^ ")"
+      ^ ")\n" ^ indent ^ "  ("
+      ^ String.concat ("\n" ^ indent ^ "   ") defs_str
+      ^ ")\n" ^ indent ^ "  " ^ string_of_expr_aux (offset + 2) body ^ ")"
   | Pair (e1, e2) ->
-      "(cons " ^ string_of_expr e1 ^ " :: " ^ string_of_expr e2 ^ ")"
+      "(cons " ^ string_of_expr_aux offset e1 ^ " :: " ^ string_of_expr_aux offset e2 ^ ")"
   | Nil -> "nil"
+
+let string_of_expr expr = string_of_expr_aux 0 expr
 
 let string_of_top_expr {value; _} = match value with
   | Expr e -> string_of_expr e
   | Define { name; expr } ->
-      "(define " ^ name ^ "\n  " ^ string_of_expr expr ^ ")"
+      "(define " ^ name ^ "\n  " ^ string_of_expr_aux 2 expr ^ ")"
 
 let rec genlist: expr list -> expr = fun l ->
   match l with
