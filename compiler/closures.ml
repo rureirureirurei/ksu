4470b5afc1eb5dfbc7b3f94d4bf84de779c59d0e
Denys Zinoviev Valerijovich <denys.zinoviev@datuum.ai>
2025-08-18 12:50:05 +0200

    Update closures module

--- compiler/closures.ml
+++ compiler/closures.ml
@@ -5,25 +5,25 @@ open Ast
 type cc_top_expr = 
 | CC_FuncDef of var * var list * cc_expr
 | CC_VarDef of var * cc_expr
+| CC_EnvDef of var * var list
 | CC_Expr of cc_expr
 
 and cc_expr =
 | CC_MakeClosure of var * cc_expr (* function id, environment - must actually be the Env*)
-| CC_MakeEnv of var list 
-| CC_EnvRef of var * var
+| CC_MakeEnv of var list * var (* env variables, env id (globalised function id)*)
+| CC_EnvRef of cc_expr * var
 | CC_AppClosure of cc_expr * cc_expr list
 (* And the regular stuff *)
 | CC_Bool of bool
 | CC_Number of int
 | CC_String of string
 | CC_Var of var
-| CC_App of cc_expr * cc_expr list
 | CC_If of cc_expr * cc_expr * cc_expr
 | CC_Callcc of cc_expr
 | CC_Let of (var * cc_expr) list * cc_expr
 | CC_Pair of cc_expr * cc_expr
 | CC_Nil
-| CC_Prim of prim
+| CC_PrimApp of prim * var list
 
 module VarSet = Set.Make(String)
 
@@ -33,7 +33,7 @@ let rec free: expr -> VarSet.t = fun expr ->
   (* Trivial Stuff *)
   | E_String _
   | E_Number _
-  | E_Prim _
+  | E_PrimApp _
   | E_Nil 
   | E_Bool _ -> VarSet.empty
   (* A bit less trivial *)
@@ -66,7 +66,7 @@ let convert: top_expr list -> cc_top_expr list =
     (* Trivial Stuff *)
     | E_String s -> CC_String s
     | E_Number n -> CC_Number n
-    | E_Prim p -> CC_Prim p
+    | E_PrimApp (p, args) -> CC_PrimApp (p, args)
     | E_Nil -> CC_Nil
     | E_Bool b -> CC_Bool b
     (* A bit trickier *)
@@ -78,15 +78,17 @@ let convert: top_expr list -> cc_top_expr list =
     | E_Pair (a, b) -> CC_Pair (t' a, t' b)
     | E_Callcc e -> CC_Callcc (t' e) 
     (* Var *)
-    | E_Var v -> (match VarSet.find_opt v sub with Some v -> CC_EnvRef (env_sym, v) | None -> CC_Var v)
+    | E_Var v -> (match VarSet.find_opt v sub with Some v -> CC_EnvRef (CC_Var env_sym, v) | None -> CC_Var v)
     (* App & Lambda *)
     | E_App (fn, args) -> CC_AppClosure (t' fn, List.map t' args)
     | E_Lambda (args, body) -> 
       let lamid = gen_lambda_id () in 
       let envid = gen_env_id () in 
-      let body' = t (free body) envid body in 
-      append (CC_FuncDef (lamid, (envid :: args), body'));
-      CC_MakeClosure (lamid, CC_MakeEnv (VarSet.to_list @@ free body))
+      let free_vars = free body in 
+      let body' = t (free body) "$env" body in 
+      append (CC_FuncDef (lamid, ("$env" :: args), body'));
+      append (CC_EnvDef (envid, VarSet.to_list free_vars));
+      CC_MakeClosure (lamid, CC_MakeEnv (VarSet.to_list free_vars, envid))
     in
 
   let t_top: top_expr -> unit = fun top_expr -> 
